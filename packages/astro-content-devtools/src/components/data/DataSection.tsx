7a0eb26bb75cf97861c96d01fe42c52fda8f59bb
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-03-24 10:19:44 +0100

    feat: data panel sticky headers

--- packages/astro-content-devtools/src/components/data/DataSection.tsx
+++ packages/astro-content-devtools/src/components/data/DataSection.tsx
@@ -4,12 +4,12 @@ import styles from './DataSection.module.css'
 
 export const DataSection: ParentComponent<DataSectionProps> = (props) => {
   return (
-    <section>
+    <>
       <h2 class={styles.title}>{props.title}</h2>
       <div class={styles.content} classList={{ [styles.horizontal!]: props.horizontal }}>
         {props.children}
       </div>
-    </section>
+    </>
   )
 }
 
